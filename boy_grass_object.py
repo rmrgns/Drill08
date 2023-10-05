57c6476b789fcc3762767c62cd5ead67cab0bfbd
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-10-05 11:24:15 +0900

    Drill 08 제출(2017180039정극훈)

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import random
 # Game object class here
 class Grass:
     def __init__(self):
@@ -11,7 +11,7 @@ class Grass:
 
 class Boy:
     def __init__(self):
-        self.x, self.y = 0, 90
+        self.x, self.y = random.randint(100, 700), 90
         self.frame = 0
         self.image = load_image('run_animation.png')
 
@@ -21,6 +21,26 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(50, 750), 599
+        self.select = random.randint(0, 1)
+        if self.select == 1:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+        self.speed = random.randint(10,50)
+    def update(self):
+        if self.y > 50:
+            self.y -= self.speed
+        else:
+            self.y = 50
+    def draw(self):
+        if self.select == 1:
+            self.image.clip_draw(0,0,23,23,self.x,self.y)
+        else:
+            self.image.clip_draw(0, 0, 43, 43, self.x, self.y)
 
 def handle_events():
     global running
@@ -34,23 +54,27 @@ def handle_events():
 def reset_world():
     global running
     global grass
-    global team
+    global ball21
     running = True
     grass = Grass()
-    team = [Boy() for i in range(10)]
+    ball21 = [Ball() for i in range(20)]
+
+
 def update_world():
     grass.update()
-    for boy in team:
-        boy.update()
+    for ball in ball21:
+        ball.update()
+
     pass
 
 def render_world():
     clear_canvas()
 
     grass.draw()
-    for boy in team:
-        boy.draw()
-    update_world()
+    for ball in ball21:
+        ball.draw()
+
+    update_canvas()
 
 open_canvas()
 
